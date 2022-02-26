ac1de94ba9f3b3d8da75ff5a9d3c545594bda4a3
Aman Kumar <amanofficial16@gmail.com>
2022-02-26 14:59:01 +0530

    updated settings

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_1')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snapshare0710.herokuapp.com']
 
 
 # Application definition
