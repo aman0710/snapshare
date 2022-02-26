b6f6afd4c84787b6e6c2806e5643d776afe863ea
Aman Kumar <amanofficial16@gmail.com>
2022-02-26 16:00:01 +0530

    upadted settings

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['snapshare0710.herokuapp.com']
 
