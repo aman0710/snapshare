82da6927154d7554d55b15991af0e92199a49efa
Aman Kumar <amanofficial16@gmail.com>
2022-02-26 13:50:13 +0530

    initial commit

---
+++ photos/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('add/', views.addPhoto, name='add'),
+]
